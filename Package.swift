5fc4a951f1887c58adaa55767e99116f03397fd1
Érico Perez Neto <ericodx@icloud.com>
2026-03-28 23:04:10 -0300

    chore: add swift-crypto dependency for Linux CryptoKit fallback

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         .plugin(name: "SwiftCPDPlugin", targets: ["SwiftCPDPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "603.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "603.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -23,6 +24,7 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "Crypto", package: "swift-crypto", condition: .when(platforms: [.linux])),
             ],
             swiftSettings: [
                 .swiftLanguageMode(.v6)
