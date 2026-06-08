36f619ecea9de91029532f6ed9f74b396cd6ccf5
Érico Perez Neto <ericodx@icloud.com>
2026-06-08 08:54:41 -0300

    refactor: add FileHasher.hash(data:) overload

--- Sources/SwiftCPD/Cache/FileHasher.swift
+++ Sources/SwiftCPD/Cache/FileHasher.swift
@@ -5,6 +5,10 @@ struct FileHasher: Sendable {
 
     func hash(contentsOf filePath: String) throws -> String {
         let data = try Data(contentsOf: URL(fileURLWithPath: filePath))
+        return hash(data: data)
+    }
+
+    func hash(data: Data) -> String {
         let digest = SHA256.hash(data: data)
         return digest.map { String(format: "%02x", $0) }.joined()
     }
